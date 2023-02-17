2f3d3e61bb2c24580e98f354b6120b26bb151359
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-02-17 02:06:47 +0000

    feat: connecting to mongo

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+mongoose.set('strictQuery', true)
+mongoose.connect(`mongodb+srv://kaue:${process.env.MONGO_PASSWORD}@cluster0.366fkjd.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => console.log('db connected'))
+    .catch(console.log)
+
+mongoose.model(
+    'Message',
+    { name: String, message: String }
+)
 
 const app = express()
 app.use(express.static(__dirname))
 
-const server = app.listen(3000, () => {
+app.listen(3000, () => {
     console.log('server running')
 })
