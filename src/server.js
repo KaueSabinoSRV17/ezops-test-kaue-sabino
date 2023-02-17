967b059c70c6421c71ed7e8e1e2747c8c8191021
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-02-17 02:32:48 +0000

    get and post routes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 require('dotenv/config')
 
 mongoose.set('strictQuery', true)
@@ -7,13 +8,29 @@ mongoose.connect(`mongodb+srv://kaue:${process.env.MONGO_PASSWORD}@cluster0.366f
     .then(() => console.log('db connected'))
     .catch(console.log)
 
-mongoose.model(
+const messageSchema = mongoose.model(
     'Message',
     { name: String, message: String }
 )
 
 const app = express()
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/messages', (request, response) => {
+    messageSchema.find({}, (error, messages) => response.send(messages))
+})
+
+app.post('/messages', (request, response) => {
+    const message = new messageSchema(request.body)
+    message.save(error => {
+        if (error) {
+            response.sendStatus(500)
+        }
+        response.sendStatus(200)
+    })
+})
 
 app.listen(3000, () => {
     console.log('server running')
